f8627f9873b7dd3b40219371355ac00f537d6a93
peo <hoangnhutsp@gmail.com>
2021-05-16 20:57:14 +0700

    mod: product.js

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -1,12 +1,55 @@
+"use strict";
+
 import Product from '../models/product.js';
 
+const DEFAULT_Q         = "";
+const DEFAULT_PAGE      = 1;
+const DEFAULT_PRICE     = "0,100000000";
+const DEFAULT_RATING    = 1;
+const DEFAULT_SORT      = "review_count,desc";
+const DEFAULT_CRITERIA  = "desc";
+
+const initQuery = (query) => {
+    let price = query.price || DEFAULT_PRICE;
+    price = String(price).split(',');
+    let minPrice = price[0];
+    let maxPrice = price[1];
+
+    let rating = query.rating || DEFAULT_RATING;
+    const queryString = {
+        price: { $gt: minPrice, $lt: maxPrice},
+        rating_average: {$gt : rating - 1},
+    }
+
+    return queryString;
+}
+
+const trans = {
+    default: 'view_count',
+    price : 'price',
+    newest: 'date',
+}
+
+const initSort = (query) => {
+    
+    let sort = String(query.sort || DEFAULT_SORT).split(',');
+    let type = sort[0];
+    let criteria = sort[1] || DEFAULT_CRITERIA;
+    const queryString = {
+        [type]: criteria
+    }
+
+    return queryString;
+}
 export const getProduct = async (req, res) => {
     try {
         const query = req.query;
 
-        console.log(query);
-        
-        const product = await Product.find();
+        const queryString = initQuery(query);
+        const sortString = initSort(query);
+
+        console.log(queryString);
+        const product = await Product.find(queryString).sort(sortString);
         res.status(200).json(product)
     } catch (error) {
         res.status(404).json({message: error.message});
