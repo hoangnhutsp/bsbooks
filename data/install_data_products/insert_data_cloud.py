5e70d9ba9ca8a6db8fc15083bed5f67b10288f0e
peo <hoangnhutsp@gmail.com>
2021-06-06 23:15:47 +0700

    mod file

--- data/install_data_products/insert_data_cloud.py
+++ data/install_data_products/insert_data_cloud.py
@@ -5,16 +5,20 @@ import json
 _key = ['products', 'product_details', 'categorys', 'configs']
 
 
-client = MongoClient("mongodb+srv://hoangnhutsp:Nhutsp240420@bsbooks.utegf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+client = MongoClient("mongodb+srv://hoangnhutsp:Nhutsp240420@bsbooks.utegf.mongodb.net/bsbooks_db?retryWrites=true&w=majority")
 
 db=client.get_database('bsbooks_db')
 
-for key in _key:
-    with open(key + '.json') as json_file:
-        data = json.load(json_file)
+products = db['products']
 
-    peo = db[key]
-    for _data in data:
-        peo.insert_one(_data)
+print(products.count_documents({}))
+
+# for key in _key:
+#     with open(key + '.json') as json_file:
+#         data = json.load(json_file)
+
+#     peo = db[key]
+#     for _data in data:
+#         peo.insert_one(_data)
 
 
