26db1d45bf892f147f8c8cb73fb727d9bb6760b2
PhuongNguyen1202 <phuong322184@gmail.com>
2021-06-13 09:42:06 +0700

    UpdateCheckout

--- client/src/page/cart/Checkout.js
+++ client/src/page/cart/Checkout.js
@@ -20,7 +20,7 @@ function Checkout() {
   const addr = [{
     id_user: "60c2668bfb6e0089781ea2b8",
     address: "Thành phố Hồ Chí Minh",
-    name: "An Nhiên",
+    name: "An Nhiên 1",
     email: "annhien@gmail.com",
     phone: "0945362763",
     is_default: 1
@@ -28,7 +28,7 @@ function Checkout() {
   {
     id_user: "60c2668bfb6e0089781ea2b8",
     address: "ĐB",
-    name: "An Nhiên",
+    name: "An Nhiên 2",
     email: "annhien@gmail.com",
     phone: "0945362764",
     is_default: 0
@@ -36,7 +36,7 @@ function Checkout() {
   {
     id_user: "60c2668bfb6e0089781ea2b8",
     address: "ĐT",
-    name: "An Nhiên",
+    name: "An Nhiên 3",
     email: "annhien@gmail.com",
     phone: "0945369765",
     is_default: 0
@@ -44,7 +44,7 @@ function Checkout() {
   {
     id_user: "60c2668bfb6e0089781ea2b8",
     address: "ĐN",
-    name: "An Nhiên",
+    name: "An Nhiên 4",
     email: "annhien@gmail.com",
     phone: "0945362766",
     is_default: 0
@@ -52,7 +52,7 @@ function Checkout() {
   {
     id_user: "60c2668bfb6e0089781ea2b8",
     address: "QT",
-    name: "An Nhiên",
+    name: "An Nhiên 5",
     email: "annhien@gmail.com",
     phone: "0945362767",
     is_default: 0
@@ -72,11 +72,11 @@ function Checkout() {
   const addressNotDefaul = addressList.filter(item => item.is_default === 0)
   const [dataAddress, setDataAddress] = useState(addressDefaul[0])
 
-//cập nhật lại số điện thoại
+  //cập nhật lại số điện thoại
   const changeAddress = (addre) => {
     let data = addressList.filter(item => item.address === addre)
-    let phone = data[0].phone
-    return phone;
+    let dataUpdate = data[0]
+    return dataUpdate;
   }
 
   useEffect(() => {
@@ -156,12 +156,6 @@ function Checkout() {
               <div className="CheckoutContainer-row">
                 <div className="col-58">
                   <h3 className="h3-checkout">Thông tin địa chỉ</h3>
-                  <label for="fname"><i className="fa fa-user"></i><span className="icon-kc">Họ và Tên</span></label>
-                  <input className="input-checkout" type="text" id="fname" name="firstname" readOnly value={infoUser.name} />
-
-                  <label for="email"><i className="fa fa-envelope"></i><span className="icon-kc">Email</span></label>
-                  <input className="input-checkout" type="text" id="email" name="email" readOnly value={infoUser.email} />
-
                   <label for="adr"><i className="fa fa-home"></i><span className="icon-kc">Địa chỉ</span></label>
                   {/* <input className="input-checkout" type="text" id="adr" name="address" value={infoUser.address} /> */}
                   <select
@@ -171,16 +165,26 @@ function Checkout() {
                     value={dataAddress.address}
                     onChange={e => {
                       //changeAddress(e.target.value);
-                      setDataAddress({...dataAddress, address: e.target.value, phone: changeAddress(e.target.value)})}}
+                      setDataAddress({ ...dataAddress, address: e.target.value, phone: changeAddress(e.target.value).phone, name: changeAddress(e.target.value).name })
+                    }}
                   >
                     <option value={addressDefaul[0].address}>{addressDefaul[0].address}</option>
                     {addressNotDefaul.map(item => {
                       return <option value={item.address}>{item.address}</option>
                     })}
                   </select>
+                  <label for="fname"><i className="fa fa-user"></i><span className="icon-kc">Họ và Tên</span></label>
+                  <input className="input-checkout"
+                    type="text" id="fname"
+                    name="firstname"
+                    value={dataAddress.name}
+                    onChange={(e) => { setDataAddress({ ...dataAddress, name: e.target.value }) }} />
 
                   <label for="city"><i className="fa fa-mobile-alt"></i><span className="icon-kc">Số điện thoại</span></label>
-                  <input className="input-checkout" type="text" id="phone" name="phone" value={dataAddress.phone} readOnly />
+                  <input className="input-checkout"
+                    type="text" id="phone"
+                    name="phone" value={dataAddress.phone}
+                    onChange={(e) => { setDataAddress({ ...dataAddress, phone: e.target.value }) }} />
                 </div>
                 <label>
                   <input type="checkbox" name="sameadr" onChange={() => clickShip(payment)} />Giao hàng nhanh trong vòng 2 giờ
